cd9871abc3f8326ae8c4f3689e35470a1aea9b2e
Ming Gao <thoughtworks.gm.gao@daimler.com>
2022-04-25 22:20:08 +0800

    Init.

---
+++ src/main/java/com/example/originalservice/OriginalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.originalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OriginalServiceApplication.class, args);
+    }
+
+}
